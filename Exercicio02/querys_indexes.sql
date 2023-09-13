b9ff7670eda504f3d51b00879403f57c8a994570
PedroAlmeida08 <74750373+PedroAlmeida08@users.noreply.github.com>
2023-09-13 19:44:12 -0300

    Fixing querys_indexes.sql

--- Exercicio02/querys_indexes.sql
+++ Exercicio02/querys_indexes.sql
@@ -1,25 +1,38 @@
-/* Índice para consultas com especificação de cidade */
-CREATE INDEX idx_cidade
-ON projeto
-USING hash(cidade);
-
+/* ------------ ÍNDICES ------------ */
+/* ---------- 1ª Consulta ---------- */
 /* Índice para consultas em um intervalo de valor */
 CREATE INDEX idx_orcamento
 ON projeto
 USING btree(orcamento);
 
-/* Índice para chave estrangeira */
+/* Índice para chave estrangeira (sempre na tabela que referencia) */
 CREATE INDEX idx_projeto
-on projeto
+on despesa
 USING hash(idprojeto);
 
-/* Índice para parâmetro usado em função de agregação*/
-CREATE INDEX idx_valor
-on despesa
-USING btree(valor);
+/* ---------- 2ª Consulta ---------- */
 
+/* também utiliza o idx-projeto */
+
+/* Índice para consultas com especificação de cidade */
+CREATE INDEX idx_cidade
+ON projeto
+USING hash(cidade);
+
+/* ---------- 3ª Consulta ---------- */
+
+/* também utiliza o idx-projeto */
+
+/* Índice para parâmetro usado em função de agregação*/
+CREATE INDEX idx_tipo_despesa
+on despesa
+USING hash(tipodespesa);
+
+/* --------------------------------- */
+
+/* ----------- CONSULTAS ----------- */
 /* A consulta deve retornar despesas de projetos com orçamento maior que 100.000,00 */
-SELECT d.valor
+SELECT d.*
   FROM despesa d, projeto p
  WHERE d.idprojeto = p.idprojeto
    AND p.orcamento > 100000;
@@ -32,7 +45,7 @@ SELECT SUM(d.valor)
 
 /* A consulta deve retornar o total de despesas de projetos do Rio de Janeiro 
 agrupado por tipo de despesa. */
-SELECT sum(d.valor)
+SELECT sum(d.valor), d.tipodespesa
   FROm despesa d, projeto p
  WHERE d.idprojeto = p.idprojeto
    AND p.cidade = 'Rio de Janeiro'
